9fa304335db454e9319dfac58e416f410eaa9196
clrkkk <81155954+clrkkk@users.noreply.github.com>
2021-03-23 10:33:49 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
+
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"jxg102700@outlook.com\"\n\
-" > ~/.streamlit/credentials.toml
-
+ 
+ 
+ 
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
